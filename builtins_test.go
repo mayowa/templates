5cf5c3054e7c058cdaa999f07d1140b276f3845a
Bosun Akinyemi <azurnwa.p@gmail.com>
2024-06-03 09:54:51 +0100

    test ifZero

--- builtins_test.go
+++ builtins_test.go
@@ -2,6 +2,59 @@ package templates
 
 import "testing"
 
+func Test__ifZero(t *testing.T) {
+	tests := []struct {
+		name   string
+		src    any
+		def    any
+		expect any
+	}{
+		{
+			name:   "src is nil",
+			src:    nil,
+			def:    "default",
+			expect: "default",
+		},
+
+		{
+			name:   "src is empty string",
+			src:    "",
+			def:    "default",
+			expect: "default",
+		},
+
+		{
+			name:   "src is zero",
+			src:    "",
+			def:    "default",
+			expect: "default",
+		},
+
+		{
+			name:   "src is not empty string",
+			src:    "src",
+			def:    "default",
+			expect: "src",
+		},
+
+		{
+			name:   "default is not str",
+			src:    "",
+			def:    0,
+			expect: 0,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ifZero(tt.src, tt.def); got != tt.expect {
+				t.Errorf("ifZero() = %v, want %v", got, tt.expect)
+			}
+		})
+	}
+
+}
+
 func Test__deDupeString(t *testing.T) {
 	tests := []struct {
 		name   string
